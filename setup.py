840242a10f31b58c1a2e5e1e88999b7ddc7e880d
Casey W. Stark <caseywstark@gmail.com>
2012-01-23 09:42:15 -0800

    distutils install script

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dimensionful', version='0.1',
+      description='',
+      author='Casey W. Stark', author_email='caseywstark@gmail.com',
+      url='http://caseywstark.com',
+      packages=['dimensionful'])
