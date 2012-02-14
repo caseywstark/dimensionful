Fixed units, quantities, and their tests. All pass now.

test/test_quantity.py
@@ -2,7 +2,7 @@
 
 Test quantity functionality.
 
-Copyright 2012, Casey W. Stark.
+Copyright 2012, Casey W. Stark. See LICENSE.txt for more information.
 
 """
 
@@ -41,27 +41,6 @@ def test_creation_with_unit_string():
     assert q1.data == 1.0
     assert q1.units == u1
 
-def test_creation_with_quantity():
-    """
-    Create a quantity object with a quantity object as ``unit``
-    parameter.
-
-    """
-    # single value version
-    u1 = Unit("Mpc")
-    q1 = Quantity(1.0, "Mpc")
-    q2 = Quantity(2.0, q1)
-
-    assert q2.data == 2.0
-    assert q2.units == u1
-
-    # array version
-    q3 = Quantity(np.arange(4), "Mpc")
-    q4 = Quantity(np.arange(4), q3)
-
-    assert (q4.data == np.arange(4)**2).all()
-    assert q4.units == u1
-
 def test_string_representation():
     """
     Check how a quantity prints.
@@ -76,9 +55,9 @@ def test_conversion():
     Convert a density quantity from CGS to cosmological units.
 
     """
-    cosmological_density_units = Unit("Msun Mpc^-3")
-    q1 = Quantity(1e-29, "g cm^-3")
-    q1.convert_to("Msun Mpc^-3")
+    cosmological_density_units = Unit("Msun * Mpc**-3")
+    q1 = Quantity(1e-29, "g * cm**-3")
+    q1.convert_to("Msun * Mpc**-3")
 
     assert equal_sigfigs(q1.data, 1.47721e11, required_precision)
     assert q1.units == cosmological_density_units
@@ -88,9 +67,9 @@ def test_get_conversion():
     Get a CGS density quantity in cosmological units.
 
     """
-    cosmological_density_units = Unit("Msun Mpc^-3")
-    q1 = Quantity(1e-29, "g cm^-3")
-    q2 = q1.get_in("Msun Mpc^-3")
+    cosmological_density_units = Unit("Msun * Mpc**-3")
+    q1 = Quantity(1e-29, "g * cm**-3")
+    q2 = q1.get_in("Msun * Mpc**-3")
 
     assert equal_sigfigs(q2.data, 1.47721e11, required_precision)
     assert q2.units == cosmological_density_units
@@ -100,14 +79,14 @@ def test_addition():
     Add two quantities.
 
     """
-    q1 = Quantity(1.0, "cm s^-1")  # plus
-    q2 = Quantity(2.0, "cm s^-1")  # equals
-    q3 = Quantity(3.0, "cm s^-1")
+    q1 = Quantity(1.0, "cm * s**-1")  # plus
+    q2 = Quantity(2.0, "cm * s**-1")  # equals
+    q3 = Quantity(3.0, "cm * s**-1")
 
     q4 = Quantity(1.0, "cm")  # wrong dimension
 
-    q5 = Quantity(1.0, "km hr^-1")
-    q6 = Quantity(.01 + 1.e3 / 3600, "m s^-1")
+    q5 = Quantity(1.0, "km * hr**-1")
+    q6 = Quantity(.01 + 1.e3 / 3600, "m * s**-1")
 
     assert q1 + q2 == q3
 
@@ -127,9 +106,9 @@ def test_subtraction():
     Subtract two quantities.
 
     """
-    q1 = Quantity(4.0, "cm s^-1")
-    q2 = Quantity(1.0, "cm s^-1")
-    q3 = Quantity(3.0, "cm s^-1")
+    q1 = Quantity(4.0, "cm * s**-1")
+    q2 = Quantity(1.0, "cm * s**-1")
+    q3 = Quantity(3.0, "cm * s**-1")
     q4 = Quantity(1.0, "cm")
 
     assert q1 - q2 == q3
@@ -140,9 +119,9 @@ def test_multiplication():
     Multiply two quantities.
 
     """
-    q1 = Quantity(2.0, "g cm^2 s^-3 K")
-    q2 = Quantity(3.0, "g cm^-2 s^-1 K")
-    q3 = Quantity(6.0, "g^2 s^-4 K^2")
+    q1 = Quantity(2.0, "g * cm**2 * s**-3 * K")
+    q2 = Quantity(3.0, "g * cm**-2 * s**-1 * K")
+    q3 = Quantity(6.0, "g**2 * s**-4 * K**2")
 
     assert q1 * q2 == q3
 
@@ -151,9 +130,9 @@ def test_division():
     Divide two quantities.
 
     """
-    q1 = Quantity(1.0, "g cm^2 s^-3 K")
-    q2 = Quantity(2.0, "g cm^-2 s^-1 K")
-    q3 = Quantity(0.5, "cm^4 s^-2")
+    q1 = Quantity(1.0, "g * cm**2 * s**-3 * K")
+    q2 = Quantity(2.0, "g * cm**-2 * s**-1 * K")
+    q3 = Quantity(0.5, "cm**4 * s**-2")
 
     assert q1 / q2 == q3
 

test/test_units.py
@@ -2,16 +2,15 @@
 
 Test unit functionality.
 
-Copyright 2012, Casey W. Stark.
+Copyright 2012, Casey W. Stark. See LICENSE.txt for more information.
 
 """
 
 import nose
-import numpy as np
 
 from utils import equal_sigfigs
 
-from dimensionful.units import Unit, num_base
+from dimensionful.units import Unit
 
 # @todo: global option?
 required_precision = 4
@@ -21,7 +20,7 @@ def test_no_conflicting_symbols():
     Construct full symbol list, check for conflicts.
 
     """
-    from dimensionful.common_units import unit_symbols_dict, unit_prefixes
+    from dimensionful.units import unit_symbols_dict, unit_prefixes
     full_set = set(unit_symbols_dict.keys())
 
     # go through all possible prefix combos
@@ -38,148 +37,113 @@ def test_no_conflicting_symbols():
 
 def test_dimensionless():
     """
-    Create dimensionless unit. Check that dimensions are all zero and string
-    representation is "(dimensionless)".
+    Create dimensionless unit.
+    Check that it thinks it is dimensionless with `is_dimensionless`.
+    Verify by checking dimensions attribute.
 
     """
     u1 = Unit()
-    assert ( (u1.dimensions == np.zeros(num_base)).all()
-             and u1.conversion_factor == 1.0
-             and u1.base_string == "(dimensionless)" )
+    assert u1.is_dimensionless and u1.dimensions == 1
 
-def test_creation_from_symbols():
+def test_creation_from_symbol_string():
     """
-    Create unit from string.
+    Create units from strings.
 
     """
-    from dimensionful.common_units import energy
+    from dimensionful.dimensions import energy
 
-    u1 = Unit("g cm^2 s^-2")
-    assert (u1.dimensions == energy).all() and u1.conversion_factor == 1.0
+    u1 = Unit("g * cm**2 * s**-2")
+    assert u1.dimensions == energy and u1.cgs_value == 1.0
 
-    u2 = Unit("cm^2 s^-2 g")  # different order
-    assert (u2.dimensions == energy).all() and u2.conversion_factor == 1.0
-
-def test_creation_from_powers():
-    """
-    Create unit using array of powers syntax.
-
-    """
-    from dimensionful.common_units import energy
-
-    u1 = Unit((1, 2, -2, 0))
-    assert (u1.dimensions == energy).all() and u1.conversion_factor == 1.0
-
-    cf = 42
-    u2 = Unit((1, 2, -2, 0), conversion_factor=cf)
-    assert (u2.dimensions == energy).all() and u2.conversion_factor == cf
+    u2 = Unit("cm**2 * s**-2 * g")  # different order
+    assert u2.dimensions == energy and u2.cgs_value == 1.0
 
 def test_creation_duplicate_dims():
     """
     Create unit with multiple symbols of the same dimension.
 
     """
-    from dimensionful.common_units import power, rate
+    from dimensionful.dimensions import power, rate
 
-    u1 = Unit("erg s^-1")
-    u2 = Unit("km s^-1 Mpc^-1")
+    u1 = Unit("erg * s**-1")
+    u2 = Unit("km * s**-1 * Mpc**-1")
     km_cgs = 1e5
     Mpc_cgs = 3.08568025e24
 
-    assert (u1.dimensions == power).all() and u1.conversion_factor == 1.0
-    assert (u2.dimensions == rate).all()
-    assert equal_sigfigs(u2.conversion_factor, km_cgs / Mpc_cgs,
-                         required_precision)
-
-def test_conversion_factor():
-    """
-    Check that conversion factors are generated and are accurate. This test is
-    a duplicate in many ways now, but it can't hurt to keep it.
-
-    """
-    from dimensionful.common_units import mass_density
-
-    Msun_cgs = 1.98892e33
-    Mpc_cgs = 3.08568025e24
-    u1 = Unit("Msun Mpc^-3")
-
-    assert (u1.dimensions == mass_density).all()
-    assert equal_sigfigs(u1.conversion_factor, Msun_cgs * Mpc_cgs**-3,
-                         required_precision)
+    assert u1.dimensions == power and u1.cgs_value == 1.0
+    assert u2.dimensions == rate
+    assert equal_sigfigs(u2.cgs_value, km_cgs / Mpc_cgs, required_precision)
 
 def test_string_representation():
     """
     Check how a unit casts to a string.
 
     """
-    from dimensionful.common_units import length
+    from dimensionful.dimensions import length
 
-    u1 = Unit("cm")
-    u2 = Unit("pc")
-    #u3 = Unit("")
+    pc = Unit("pc")
+    Myr = Unit("Myr")
+    speed = pc / Myr
 
-    assert str(u1) == "cm"
-    assert str(u2) == "pc"
-    #assert str(u3) == "pc"
+    assert str(pc) == "pc"
+    assert str(Myr) == "Myr"
+    assert str(speed) == "pc/Myr"
 
 def test_multiplication():
     """
     Multiply two units.
 
     """
-    from dimensionful.common_units import momentum
+    from dimensionful.dimensions import momentum
 
     Msun_cgs = 1.98892e33
     pc_cgs = 3.08568025e18
-    u1 = Unit("Msun s^-1")
+    u1 = Unit("Msun * s**-1")
     u2 = Unit("pc")
 
     u3 = u1 * u2
 
-    assert (u3.dimensions == momentum).all()
-    assert equal_sigfigs(u3.conversion_factor, Msun_cgs * pc_cgs,
-                         required_precision)
+    assert u3.dimensions == momentum
+    assert equal_sigfigs(u3.cgs_value, Msun_cgs * pc_cgs, required_precision)
 
 def test_division():
     """
     Divide two units.
 
     """
-    from dimensionful.common_units import rate
+    from dimensionful.dimensions import rate
 
     pc_cgs = 3.08568025e18
     km_cgs = 1e5
     u1 = Unit("pc")
-    u2 = Unit("km s")
+    u2 = Unit("km * s")
 
     u3 = u1 / u2
 
-    assert (u3.dimensions == rate).all()
-    assert equal_sigfigs(u3.conversion_factor, pc_cgs / km_cgs,
-                         required_precision)
+    assert u3.dimensions == rate
+    assert equal_sigfigs(u3.cgs_value, pc_cgs / km_cgs, required_precision)
 
 def test_pow():
     """
     Take units to some power.
 
     """
+    from dimensionful.dimensions import mass, length, time, temperature
+
     pc_cgs = 3.08568025e18
     mK_cgs = 1e-3
-    u1_powers = np.array((1, 2, -3, 4))
-    u1 = Unit("g pc^2 s^-3 mK^4")
+    u1_dims = mass * length**2 * time**-3 * temperature**4
+    u1 = Unit("g * pc**2 * s**-3 * mK**4")
 
     u2 = u1**2
 
-    print "%e" % u2.conversion_factor
-    print "%e" % (pc_cgs**2 * mK_cgs**4)**2
-
-    assert (u2.dimensions == u1_powers*2).all()
-    assert equal_sigfigs(u2.conversion_factor, (pc_cgs**2 * mK_cgs**4)**2,
+    assert u2.dimensions == u1_dims**2
+    assert equal_sigfigs(u2.cgs_value, (pc_cgs**2 * mK_cgs**4)**2,
                          required_precision)
 
     u3 = u1**(-1./3)
-    assert (u3.dimensions == -1. / 3 * u1_powers).all()
-    assert equal_sigfigs(u3.conversion_factor, (pc_cgs**2 * mK_cgs**4)**(-1./3),
+    assert u3.dimensions == u1_dims**(-1./3)
+    assert equal_sigfigs(u3.cgs_value, (pc_cgs**2 * mK_cgs**4)**(-1./3),
                          required_precision)
 
 def test_equality():
@@ -187,7 +151,7 @@ def test_equality():
     Verify unit equality checks.
 
     """
-    u1 = Unit("km s^-1")
-    u2 = Unit("m ms^-1")
+    u1 = Unit("km * s**-1")
+    u2 = Unit("m * ms**-1")
 
     assert u1 == u2