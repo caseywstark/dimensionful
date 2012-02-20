bdeef713d2d88289f264287c5296daea2b937977
Casey W. Stark <caseywstark@gmail.com>
2012-02-20 10:50:47 -0800

    constants: add a few.

--- dimensionful/constants.py
+++ dimensionful/constants.py
@@ -18,8 +18,13 @@ c = Quantity(2.99792458e10, cm / s)
 G = Quantity(6.673e-8, cm**3 * g**-1 * s**-2)
 
 # Boltzmann constant
-k = Quantity(1.38064e-16, erg * K)
+k = Quantity(1.38064e-16, erg / K)
 
 # Planck constant
 h = Quantity(6.626070e-27, erg * s)
 hbar = h / (2 * np.pi)
+
+e = Quantity(4.8032068e-10, esu)
+m_p = Quantity(1.672623e-24, g)
+m_e = Quantity(9.109389e-28, g)
+amu = Quantity(1.6605402e-24, g)
