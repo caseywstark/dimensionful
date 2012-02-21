ce5a5060cf889fc86230bb8ed6fdef83dd8ed6e7
Casey W. Stark <caseywstark@gmail.com>
2012-02-21 15:29:51 -0800

    Added and cleaned up Unit tests.

--- test/test_units.py
+++ test/test_units.py
@@ -35,7 +35,9 @@ def test_no_conflicting_symbols():
 
             full_set.add(new_symbol)
 
-def test_create_dimensionless():
+### Begin creation tests
+
+def test_creation_dimensionless():
     """
     Create dimensionless unit.
     Check that it thinks it is dimensionless with `is_dimensionless`.
@@ -57,10 +59,13 @@ def test_creation_from_string():
     from dimensionful.dimensions import energy
 
     u1 = Unit("g * cm**2 * s**-2")
-    assert u1.dimensions == energy and u1.cgs_value == 1.0
+    assert u1.dimensions == energy
+    assert u1.cgs_value == 1.0
 
-    u2 = Unit("cm**2 * s**-2 * g")  # different order
-    assert u2.dimensions == energy and u2.cgs_value == 1.0
+    # make sure order doesn't matter
+    u2 = Unit("cm**2 * s**-2 * g")
+    assert u2.dimensions == energy
+    assert u2.cgs_value == 1.0
 
 def test_creation_from_expr():
     """
@@ -72,66 +77,37 @@ def test_creation_from_expr():
     pc_cgs = 3.08568e18
     yr_cgs = 31536000
 
+    # Symbol expr
     s1 = Symbol("pc")
     s2 = Symbol("yr")
-    s3 = s1 / s2
+
+    # Mul expr
+    s3 = s1 * s2
+
+    # Pow expr
+    s4  = s1**2 * s2**(-1)
 
     u1 = Unit(s1)
     u2 = Unit(s2)
-    u3 = Unit(s1 / s2)
+    u3 = Unit(s3)
+    u4 = Unit(s4)
 
     assert u1.expr == s1
     assert u2.expr == s2
     assert u3.expr == s3
+    assert u4.expr == s4
 
     assert u1.cgs_value == pc_cgs
     assert u2.cgs_value == yr_cgs
-    assert u3.cgs_value == pc_cgs / yr_cgs
+    assert u3.cgs_value == pc_cgs * yr_cgs
+    assert u4.cgs_value == pc_cgs**2 / yr_cgs
 
     assert u1.dimensions == length
     assert u2.dimensions == time
-    assert u3.dimensions == length / time
+    assert u3.dimensions == length * time
+    assert u4.dimensions == length**2 / time
 
-# @todo: real Exception type
-def test_creation_argument_type():
-    """
-    Create a unit with a bad argument for the symbol expr.
-
-    """
-    from sympy import Matrix
-
-    # python type that is not str
-    try:
-        u1 = Unit([1])
-    except Exception:
-        pass
-
-    # sympy type that is not a valid unit Expr
-    try:
-        u1 = Unit(Matrix([[1,0], [0,1]]))
-    except Exception:
-        pass
-
-def test_creation_unknown_symbol():
-    """
-    Create with a bad symbol.
-
-    """
-    from sympy import Symbol
-
-    # Symbol version
-    try:
-        u1 = Unit(Symbol("jigawatts"))
-    except Exception:
-        pass
-
-    # string version
-    try:
-        u1 = Unit("metricfuckton")
-    except Exception:
-        pass
-
-def test_creation_with_duplicate_dims():
+def test_creation_expr_with_duplicate_dimensions():
     """
     Create unit with multiple symbols of the same dimension.
 
@@ -148,6 +124,119 @@ def test_creation_with_duplicate_dims():
 
     assert u2.cgs_value == km_cgs / Mpc_cgs
     assert u2.dimensions == rate
+
+def test_creation_new_symbol():
+    """
+    Create custom Unit with unknown symbol, giving cgs value and dimensions.
+
+    """
+    from dimensionful.dimensions import mass, time
+    from sympy import Symbol
+
+    u1 = Unit("abc", cgs_value=42, dimensions=(mass/time))
+
+    assert u1.expr == Symbol("abc")
+    assert u1.cgs_value == 42
+    assert u1.dimensions == mass / time
+
+# @todo: real Exception type, real exception assertions
+def test_creation_fail_on_bad_expr_type():
+    """
+    Create a unit with a bad argument for the symbol expr.
+
+    """
+    from sympy import Matrix
+
+    # python type that is not str
+    try:
+        u1 = Unit([1])
+    except Exception:
+        pass
+    else:
+        assert False
+
+    # sympy type that is not a valid unit Expr
+    try:
+        u1 = Unit(Matrix([[1,0], [0,1]]))
+    except Exception:
+        pass
+    else:
+        assert False
+
+def test_creation_fail_on_unknown_symbol():
+    """
+    Create with a bad symbol.
+
+    """
+    from sympy import Symbol
+
+    # Symbol version
+    try:
+        u1 = Unit(Symbol("jigawatts"))
+    except Exception:
+        pass
+    else:
+        assert False
+
+    # string version
+    try:
+        u1 = Unit("metricfckton")
+    except Exception:
+        pass
+    else:
+        assert False
+
+def test_creation_fail_on_dimensions_type():
+    """
+    Create custom Unit, but give the wrong type for dimensions.
+
+    """
+    from sympy import Symbol
+
+    abc = Symbol("abc")
+
+    try:
+        u1 = Unit(abc, cgs_value=1, dimensions=4)
+    except Exception:
+        pass
+    else:
+        assert False
+
+def test_creation_fail_on_dimensions_content():
+    """
+    Create custom Unit, but give invalid dimensions expr.
+
+    """
+    from sympy import Symbol
+
+    abc = Symbol("abc")
+
+    try:
+        u1 = Unit(abc, cgs_value=1, dimensions=abc)
+    except Exception:
+        pass
+    else:
+        assert False
+
+def test_creation_fail_on_cgs_value_type():
+    """
+    Create custom Unit, but give bad cgs_value type.
+
+    """
+    from dimensionful.dimensions import mass, time
+    from sympy import Symbol
+
+    abc = Symbol("abc")
+
+    try:
+        u1 = Unit(abc, cgs_value=abc, dimensions=(mass/time))
+    except Exception:
+        pass
+    else:
+        assert False
+
+### End creation tests
+
 
 def test_string_representation():
     """
@@ -167,22 +256,43 @@ def test_string_representation():
     assert repr(speed) == "pc/Myr"
     assert str(dimensionless) == "(dimensionless)"
 
+### Unit operations
 def test_multiplication():
     """
     Multiply two units.
 
     """
-    from dimensionful.dimensions import momentum
-
+    from sympy import Symbol
+    from dimensionful.dimensions import mass, length, time
+    from dimensionful.common_units import Msun, pc
     Msun_cgs = 1.98892e33
     pc_cgs = 3.08568e18
-    u1 = Unit("Msun * s**-1")
+
+    # Symbol times Symbol
+    u1 = Unit("Msun")
     u2 = Unit("pc")
 
     u3 = u1 * u2
 
-    assert u3.dimensions == momentum
+    # @todo: better expression comparison method. For now, we have to extract
+    # the same Symbols used in the multiplication to get equivalence...
+    u3_expr = u1.expr * u2.expr
+
+    assert u3.expr == u3_expr
     assert u3.cgs_value == Msun_cgs * pc_cgs
+    assert u3.dimensions == mass * length
+
+    # Pow times Mul
+    u4 = Unit("pc**2")
+    u5 = Unit("Msun * s")
+
+    u6 = u4 * u5
+
+    u6_expr = u4.expr * u5.expr
+
+    assert u6.expr == u6_expr
+    assert u6.cgs_value == pc_cgs**2 * Msun_cgs
+    assert u6.dimensions == length**2 * mass * time
 
 def test_division():
     """
@@ -190,16 +300,20 @@ def test_division():
 
     """
     from dimensionful.dimensions import rate
-
     pc_cgs = 3.08568e18
     km_cgs = 1e5
+
     u1 = Unit("pc")
     u2 = Unit("km * s")
 
     u3 = u1 / u2
 
-    assert u3.dimensions == rate
+    # @todo: see mul test above
+    u3_expr = u1.expr / u2.expr
+
+    assert u3.expr == u3_expr
     assert u3.cgs_value == pc_cgs / km_cgs
+    assert u3.dimensions == rate
 
 def test_power():
     """
